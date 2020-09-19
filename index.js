a7e8c2652b8afd533164ba5c463bc84c73ebfeb8
Ruggia <angelruggia@gmail.com>
2020-09-18 21:48:34 -0300

    Tecnicamente ya se puede jugar pero faltan unas cositas

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { emit } = require('process');
 app.set('port', process.env.PORT || 4000);
 
 //Static files
-//app.use(express.static(path.join(__dirname,'frontend'))); //path.join une el directorio actual con la carpeta public. Esto porque en windows y linux se utiliza / o \.
+app.use(express.static(path.join(__dirname,'public'))); //path.join une el directorio actual con la carpeta public. Esto porque en windows y linux se utiliza / o \.
 
 //Star server
 const server = app.listen(app.get('port'), () => {
@@ -20,28 +20,71 @@ const io = SocketIO(server);
 
 let jugadoresMesa = [];
 let cantidadOn = 0;
-let Cartas = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+let Cartas = [3,4,5,6,7,8,9,3,4,5,6,7,8,9,3,4,5,6,7,8,9,3,4,5,6,7,8,9,3,4,5,6,7,8,9,3,4,5,6,7,8,9,];
 let turno = 0;
+
+//Croupier
+let croupier = {
+    nombre: "Croupier Ruggia",
+    cartas: [],
+    total: 0,
+}
+let cartaX;
+
+
 io.on('connection', (socket) =>{
+    const actualizar = () =>{
+        io.sockets.emit('actualizaCantidaOn',{
+            cantidadOn: cantidadOn,
+            jugadoresMesa: jugadoresMesa,
+            croupier: croupier
+        })
+    }
+    const comprobarFinalizacion = () =>{
+        console.log(turno,jugadoresMesa.length)
+        if(turno+1 === jugadoresMesa.length){
+            console.log("paso 1",cartaX);
+            croupier.total = croupier.cartas[0]+cartaX;
+            croupier.cartas[1] = cartaX;
+            while(croupier.total<=16){
+                let pop = Cartas.pop();
+                croupier.total += pop;
+                croupier.cartas.push(pop);
+            }
+            for(let x = 0 ; x < jugadoresMesa.length; x++){
+                if(jugadoresMesa[x].total>=croupier.total && jugadoresMesa[x].total<=21 || croupier.total>21 && jugadoresMesa[x].total<=21){
+                    jugadoresMesa[x].gana = true;
+                }else{
+                    jugadoresMesa[x].pierde = true;
+                }
+            }
+            console.log(jugadoresMesa)
+            actualizar();
+        }else{
+            turno++;
+            io.to(jugadoresMesa[turno].id).emit('darTurno');
+        }
+    }
     console.log("alguien se conectó, su id es: ", socket.id);
     
-    //io.sockets.emit('actualizaCantidaOn', cantidadOn);
-    io.to(socket.id).emit('actualizaCantidaOn', cantidadOn);
+    actualizar();
     
     //Un usuario intenta entrar en la mesa para jugar, se incrementa el contador
     socket.on('entrarMesa', (value) =>{
         let mensaje = 1;
         //Si value es 1 significa que se presionó el boton entrar
         //Si value es 1 significa que se presionó el boton salir
-        console.log(value)
         if(value.codigo === 1){
             //Si la cantidad es menor, dejamos entrar sino le damos un mensaje
-            if(cantidadOn <= 2){
+            if(cantidadOn <= 5){
                 //Agrega el id de un cliente al arreglo
                 jugadoresMesa.push({
                     nombre: value.nombre,
                     id: socket.id.toString(),
-                    cartas: []
+                    cartas: [],
+                    total: 0,
+                    pierde: false,
+                    gana: false,
                 });
                 cantidadOn++;
 
@@ -50,47 +93,95 @@ io.on('connection', (socket) =>{
                 mensaje = 0;
             }
         }else{
-            cantidadOn--;
-            
             mensaje = 1;
+            salirMesa();
         }
         io.sockets.emit('entrarMesa', {
             mensaje, mensaje,
-            cantidad: cantidadOn,
-            jugadoresMesa: jugadoresMesa
+            cantidadOn: cantidadOn,
+            jugadoresMesa: jugadoresMesa,
+            croupier: croupier
         }); 
     })
 
+    //Cuando se desconecta alguien, tenemos que saber si estaba en la mesa para sacarlo
     socket.on('disconnect', () =>{
         console.log("se desconectó: ",socket.id);
         
         if(jugadoresMesa.some((value) => (value.id === socket.id.toString()))){
-            //Saca a un cliente del arreglo cuando se desconecta
-            jugadoresMesa = jugadoresMesa.filter((value) => (socket.id.toString() !== value.id));
-            console.log("hola")
-            //Decrementa la cantidad cuando el cliente sale de la sala o se desconecta
-            cantidadOn--;
-            io.sockets.emit('actualizaCantidaOn', cantidadOn);
-            io.sockets.emit('jugar', jugadoresMesa);
+            salirMesa();
         }                 
     })
 
     socket.on('jugar', ()=> {
         if(turno < 2){
-            let cartasRepartidas = [];
             for (let j = 0; j < 2; j++) {
-                let ar = [];
                 for (let i = 0; i < jugadoresMesa.length; i++) {
-                    //ar.push(Cartas.pop())
                     jugadoresMesa[i].cartas.push(Cartas.pop());
-                
-                }  
-                cartasRepartidas.push()
+                    jugadoresMesa[i].total +=  jugadoresMesa[i].cartas[jugadoresMesa[i].cartas.length-1];
+                    if(jugadoresMesa[i].total>21){
+                        jugadoresMesa[i].pierde = true;
+                    }
+                }
+                if(j === 1){
+                    cartaX = Cartas.pop();
+                    croupier.cartas.push(0);
+                }
+                else{
+                    croupier.cartas.push(Cartas.pop());
+                }
+
             }
-            console.log("object")
-            io.sockets.emit('jugar', jugadoresMesa);
+            io.sockets.emit('jugar', {
+                jugadoresMesa: jugadoresMesa,
+                croupier: croupier
+            });
+        }
+        let i = jugadoresMesa.findIndex((el) => el.total<21);
+        if(i != -1){
+            turno=i;
+            console.log(jugadoresMesa[i].total,jugadoresMesa[i].nombre)
+            io.to(jugadoresMesa[i].id).emit('darTurno');
+        }
+        else
+        console.log("todos ganaron");
+    })
+
+    socket.on('accionTurno', (data) =>{
+        if(jugadoresMesa[turno].id === socket.id){
+            if(data.accion === "pedir" && jugadoresMesa[turno].total<21){
+                jugadoresMesa[turno].cartas.push(Cartas.pop());
+                jugadoresMesa[turno].total +=  jugadoresMesa[turno].cartas[jugadoresMesa[turno].cartas.length-1];
+
+                
+                if(jugadoresMesa[turno].total<=21){
+                    actualizar()
+                    io.to(jugadoresMesa[turno].id).emit('darTurno');
+                }
+                else{
+                    jugadoresMesa[turno].pierde = true;
+                    actualizar();
+                    
+                    comprobarFinalizacion();
+                }                
+            }else{
+                comprobarFinalizacion();                
+            }
+            
         }
     })
+    
+    const salirMesa = () => {
+        //Saca a un cliente del arreglo cuando se desconecta
+        jugadoresMesa = jugadoresMesa.filter((value) => (socket.id.toString() !== value.id));
+        //Decrementa la cantidad cuando el cliente sale de la sala o se desconecta
+        cantidadOn--;
+        io.sockets.emit('actualizaCantidaOn', {
+            cantidadOn: cantidadOn,
+            jugadoresMesa: jugadoresMesa,
+            croupier: croupier
+        });
+    }
     
 })
 
--- src/App.js
+++ src/App.js
@@ -1,84 +1,156 @@
 import React,{useEffect, useState} from 'react';
 import io from 'socket.io-client';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+
 const socket = io.connect('http://localhost:4000');
+ 
+
 
 function App() {
   const [cantidadOn, setcantidadOn] = useState(0);
   const [conectado, setconectado] = useState(false);
-  const [nombre, setnombre] = useState("Jugador");
+  const [nombre, setnombre] = useState("Unnamed");
   const [jugadores, setjugadores] = useState([]);
+  const [croupier, setcroupier] = useState();
+  const [turno, setturno] = useState(false);
+  const [id, setid] = useState("");
 
-  useEffect(() => {
+
+  useEffect(() => { 
     socket.on('actualizaCantidaOn', (data) => {
-      setcantidadOn(data);
-      let jugador = "Jugador: "+data;
-      setnombre(jugador);
+      actualizarDatos(data);
     })
     //Obtiene la cantidad de usuarios conectados en la mesa
     socket.on('entrarMesa', function(data) {
       if(data.mensaje === 1){
-        setcantidadOn(data.cantidad);
-        setjugadores(data.jugadoresMesa);
-        console.log(data.jugadoresMesa);
+        actualizarDatos(data);
       }else{
         alert("No entraste a la mesa debido a que esta llena")
       }
     })
 
     socket.on('jugar', (data) => {
-      setjugadores(data);
-      console.log(data);
+      setjugadores(data.jugadoresMesa);
+      setcroupier(data.croupier);
+    })
+
+    socket.on('darTurno',() =>{
+      setturno(true);
     })
   },[])
   
-  const entrarMesa = (e) =>{
-    //Hace algo segun si el boton entrar o salir es presionado
-    if(e.target.name === "entrar"){
-      setconectado(true);
-      setnombre("Jugador: "+cantidadOn);
-
-      socket.emit('entrarMesa',{
-        codigo: 1,
-        nombre: nombre
-      });
-      
-    }
-    if(e.target.name === "salir"){
-      setconectado(false);
-      socket.emit('entrarMesa',{
-        codigo: 0,
-        nombre: nombre
-      });
-    }
-    
+  const actualizarDatos = (data) => {
+    setcantidadOn(data.cantidadOn);
+    setjugadores(data.jugadoresMesa);
+    console.log(data.jugadoresMesa);
+    setcroupier(data.croupier);
   }
+
+  const entrarMesa = () =>{
+    setconectado(true);
+
+    socket.emit('entrarMesa',{
+      codigo: 1,
+      nombre: nombre
+    });
+
+  }
+  const salirMesa = () =>{
+    setconectado(false);
+    socket.emit('entrarMesa',{
+      codigo: 0,
+      nombre: nombre
+    });    
+  }
+
   const jugar = () => {
     socket.emit('jugar', "asd");
   }
   const cambiaNombre = (e) => {
     setnombre(e.target.value);
-    console.log(nombre)
   }
+  const Crp = () => {
+    if(croupier){
+      return <Carta jugador={croupier}/>
+    }
+  }
+
+  const accionTurno = (e) => {
+    setturno(false);
+    socket.emit('accionTurno', {accion: e.target.name});
+  }
+
   return (
     <div className="App">
-      <input type="text" onChange={cambiaNombre} value={nombre}></input>
+      <Paper style={{width:"auto", maxWidth: 950,background:"#34495E",margin:"auto",display:"flex",justifyContent: 'center',alignItems: 'center',flexWrap: 'wrap',padding: 10}}>
+        <Grid direction="row" container justify="center" spacing={1}>
+          <Grid item xs={12}>
+            <Typography variant="h4" component="h1" align="left">
+              BlackJack
+            </Typography>
+            <Typography variant="h5" component="h1" align="left">
+              {`Conectados en la mesa: ${cantidadOn}`}
+            </Typography>          
+          </Grid>
+          
+          <Grid item xs={12}>
+            <TextField disabled={conectado} onChange={cambiaNombre} value={nombre} label="Nombre de usuario" variant="filled" required/>
+          </Grid>
 
-      <button disabled={!conectado} name="salir" onClick={entrarMesa}>Salir de la mesa</button>
-      <button disabled={cantidadOn>2 || conectado} name="entrar" onClick={entrarMesa}>Entrar a la mesa</button>
-      <div>
-        {
-          jugadores.map((jugador, i) => (
-            <Carta jugador={jugador} key={i}/>
-          ))
-        }
-      </div>
-      <p> 
-        Conectados en la mesa: 
-        {cantidadOn}
-      </p>
-      <hr/>
-      <button onClick={jugar}>Juguemos</button>
+          <Grid item xs={12}>
+            <Button disabled={cantidadOn>4 || conectado} onClick={entrarMesa} variant="contained" color="primary">Entrar a la mesa</Button>
+            <Button disabled={!conectado} onClick={salirMesa} variant="contained" color="primary">Salir de la mesa</Button>
+          </Grid>
 
+          <Grid item xs={12}>
+            <Paper style={{...Estilos.paper, background:"#8CC139"}} elevation={2}>
+              {
+                croupier &&
+                <Carta jugador={croupier}/>
+              }  
+            </Paper>
+          </Grid>
+          <Grid direction="row" container justify="center" spacing={2}>
+          {
+            jugadores.map((jugador, i) => (
+              <Grid item xs={12} sm={4}>
+                <Paper style={{...Estilos.paper, background: jugador.pierde?"red":"white"}} elevation={2}>
+                  <h1>{jugador.nombre}</h1>
+                  {
+                    jugador.gana === true &&
+                    <h1>¡Felicidades ganaste!</h1>
+                  }
+                  <h5>Cartas:</h5>
+                  {
+                    jugador.cartas.map((carta, i) => (
+                      <h5 key={i}>{carta}</h5>
+                    ))
+                  }
+                  <h5>Total: {jugador.total}</h5>
+                  {
+                    jugador.nombre === nombre &&
+                    <div hidden={!turno}>
+                      <button name="pedir" onClick={accionTurno}>Pedir</button>
+                      <button name="pasar" onClick={accionTurno}>Pasar</button>
+                      <button name="doblar" onClick={accionTurno}>Doblar</button>
+                    </div>
+                  }
+                </Paper>
+              </Grid> 
+            ))
+          }
+          </Grid>
+
+          <Grid item xs={12}>
+            <button disabled={!conectado} onClick={jugar}>Juguemos</button>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 }
@@ -87,14 +159,23 @@ function Carta({jugador}) {
   return (
     <div>
       <h1>{jugador.nombre}</h1>
+      <h5>Cartas:</h5>
       {
         jugador.cartas.map((carta, i) => (
           <h5 key={i}>{carta}</h5>
         ))
       }
+      <h5>Total: {jugador.total}</h5>
     </div>
   )
 }
 
+const Estilos = {
+  paper:{
+    maxWidth:280,
+    margin: "auto",
+    padding: 10,
+  }
+}
 
 export default App;
