a7ee044b4a0dbcf8f0fb3d03e4fc36406f8c2e88
Ruggia <angelruggia@gmail.com>
2020-12-19 20:08:15 -0300

    Updates

---
+++ precache-manifest.09f27caf289358c6b79731c40b7d3e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa8271f2c430a77e88c47aea6b59210",
+    "url": "/BlackJack/index.html"
+  },
+  {
+    "revision": "019fbfb76eaa4f45d5a6",
+    "url": "/BlackJack/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "779700be6123e1418ea8",
+    "url": "/BlackJack/static/js/2.767a0b99.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/BlackJack/static/js/2.767a0b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019fbfb76eaa4f45d5a6",
+    "url": "/BlackJack/static/js/main.0a5557bc.chunk.js"
+  },
+  {
+    "revision": "749cb502ab6580b3c026",
+    "url": "/BlackJack/static/js/runtime-main.75e7d9db.js"
+  }
+]);
